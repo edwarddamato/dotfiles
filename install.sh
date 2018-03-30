862f5220ad03d3ae5a72ab77b02dd2bb8d46ef0f
Edward D'Amato <info@edwarddamato.com>
2018-03-30 18:43:53 +0100

    Disable gpg for git

--- install.sh
+++ install.sh
@@ -6,5 +6,24 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # git
 . ./git/install.sh $BASEDIR
 
+# vscode
+# - settings
+# - extensions
+
+# utils
+# - jumpcut
+# - shiftit
+# - postman
+# - spotify
+# - 1password
+# - slack
+# - chrome
+# - firefox
+# - skype
+
+# os
+# - terminal
+# - settings
+
 # done
 echo 'All done! 👍'
